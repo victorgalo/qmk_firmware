814dd7794551853b340a48b3229ed791636d3efb
victorgalo <vtrgalo@gmail.com>
2024-09-12 07:56:40 +0200

    Add ymdk id75 keymap, first version

--- keyboards/ymdk/id75/config.h
+++ keyboards/ymdk/id75/config.h
@@ -86,5 +86,25 @@
 #define ENABLE_RGB_MATRIX_MULTISPLASH
 #define ENABLE_RGB_MATRIX_SOLID_SPLASH
 #define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
-    
+
+
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
+// #define LEADER_PER_KEY_TIMING
+#define LEADER_NO_TIMEOUT
+#define LEADER_KEY_STRICT_KEY_PROCESSING
+#define LEADER_TIMEOUT 0
+
+#define CAPS_WORD_INVERT_ON_SHIFT
+#define CAPS_WORD_IDLE_TIMEOUT 5000
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+// #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+
+#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+
 #endif
