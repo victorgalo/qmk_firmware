f628f8c139be449ad452f8a47bab271efebf4cb4
victorgalo <vtrgalo@gmail.com>
2025-03-02 01:56:31 +0100

    Add custom keymap for keychron v4 iso

---
+++ keyboards/keychron/v4/iso/rules.mk
@@ -0,0 +1,21 @@
+# Build Options
+#   change yes to no to disable.
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable USB N-key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+DIP_SWITCH_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+UNICODE_ENABLE = yes
+LEADER_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+COMMAND_ENABLE = no
+PERMISSIVE_HOLD = yes
+
