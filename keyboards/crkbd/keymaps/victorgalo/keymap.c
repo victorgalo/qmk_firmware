6dc3539d8185b84acd26c6b3ec3a755f78c691d9
victorgalo <vtrgalo@gmail.com>
2025-03-23 21:41:14 +0100

    Add settings for leader key

--- keyboards/crkbd/keymaps/victorgalo/keymap.c
+++ keyboards/crkbd/keymaps/victorgalo/keymap.c
@@ -424,8 +424,6 @@ void send_spanish_accent(uint8_t letter) {
             unregister_code(KC_LALT);
             tap_code(vowel_letter_key_map[letter]);
         }
-        SEND_STRING("maaaa");
-
     } else if (operativeSystem == OS_LIN) {
         if (isCaps) {
             tap_code(KC_CAPS_LOCK);
@@ -438,7 +436,6 @@ void send_spanish_accent(uint8_t letter) {
             tap_code(vowel_letter_key_map[letter]);
             unregister_code(KC_RALT);
         }
-        SEND_STRING("liiiii");
 
     } else {
         decimal_unicode_in = isCaps ? spanish_accent_decimal_unicodes[letter][1] : spanish_accent_decimal_unicodes[letter][0];
@@ -446,8 +443,6 @@ void send_spanish_accent(uint8_t letter) {
         register_code(KC_LALT);
         tap_win_alt_code();
         unregister_code(KC_LALT);
-        SEND_STRING("wiiiii");
-
     }
 }
 
