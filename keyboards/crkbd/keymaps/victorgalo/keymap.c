d1e6cbb1d299bf3ccb570002165cc3678b48af0a
victorgalo <vtrgalo@gmail.com>
2025-03-27 22:01:40 +0100

    Change caps word to not convert - into _

--- keyboards/crkbd/keymaps/victorgalo/keymap.c
+++ keyboards/crkbd/keymaps/victorgalo/keymap.c
@@ -512,12 +512,14 @@ bool caps_word_press_user(uint16_t keycode) {
   switch (keycode) {
     // Keycodes that continue Caps Word, with shift applied.
     case KC_A ... KC_Z:
-    case KC_MINS:
-      // Apply shift to the next key.
-      add_weak_mods(MOD_BIT(KC_LSFT));
-      return true;
+
+    // case KC_MINS:
+    //   // Apply shift to the next key.
+    //   add_weak_mods(MOD_BIT(KC_LSFT));
+    //   return true;
 
     // Keycodes that continue Caps Word, without shifting.
+    case KC_MINS:
     case KC_1 ... KC_0:
     case KC_BSPC:
     case KC_DEL:
